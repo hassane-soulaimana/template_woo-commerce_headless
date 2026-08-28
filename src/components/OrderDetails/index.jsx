b50c5f12800cf6fd5df1a6f5501e482cb1746096
hassane-soulaimana <hassane.soulaimana@laplateforme.io>
2026-08-28 11:46:44 +0200

    css (couleur + modal)

--- src/components/OrderDetails/index.jsx
+++ src/components/OrderDetails/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import SucessMessage from "../SucessMessage";
+import "./index.scss";
 
 const ordersCache = {};
 
